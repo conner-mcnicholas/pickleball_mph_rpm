def597506edd450019991d7355b38d1162cb9f28
Conner McNicholas <connermcnicholas@gmail.com>
2023-10-13 09:46:03 -0700

    first pass

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ball speed & spin rate calculator
+ 
+Provided the footage was shot with a high enough resolution and frame rate to discern the ball's rotation from 1 frame to the next, and we know what that frame rate is, we can determine the ball's spin rate in RPM (revolutions per minute)
+
+![alt text](https://github.com/conner-mcnicholas/pickleball_mph_rpm/blob/main/imgs/rpm.png?raw=true)
+
+By estimating the distance a shot traveled in all 3 dimensions, and we know what that frame rate is, we can determine the ball's speed in MPH (miles per hour)
+
+![alt text](https://github.com/conner-mcnicholas/pickleball_mph_rpm/blob/main/imgs/mph.png?raw=true)
+
+
+This script just spares us the arithmetic...
+
+
+![alt text](https://github.com/conner-mcnicholas/pickleball_mph_rpm/blob/main/imgs/calcslide.png?raw=true)
+
+
+
---
+++ mphrpm.py
@@ -0,0 +1,31 @@
+def mph(dx,dy,dz,frames,framerate):
+    d = ((dx**2)+(dy**2)+(dz**2))**.5
+    t = frames/framerate
+    fps = d/t
+    mph = fps*3600/5280
+    print(f'\n\t\tspeed = {round(mph,2)} MPH')
+
+def rpm(f,fr):
+    rpm = fr*60/f
+    print(f'\n\t\tspin rate = {round(rpm,2)} RPM')
+
+def prompt_calc():
+    while True:
+        metric = str.upper(input('Input "RPM" or "MPH"\n\t'))
+        if metric == 'MPH':
+            x = float(input('\nDistance traveled along the baseline axis = '))
+            y = float(input('Distance traveled along the sideline axis = '))
+            z = float(input('Distance traveled vertically = '))
+            f = float(input('Elapsed frames = '))
+            fr = float(input('Frame rate = '))
+            mph(x,y,z,f,fr)
+            break
+        elif metric == 'RPM':
+            f = float(input('\nElapsed frames for 1 revolution = '))
+            fr = float(input('Frame rate = '))
+            rpm(f,fr)
+            break
+        else:
+            print("\nInput not recognized.  Let's try that again, shall we?\n\n")    
+
+prompt_calc()
